27e8f0ecfe51bd620fd5569e4ea6abb21b14c69b
Moniruzzaman Rishad <mzrishad.info@gmail.com>
2025-07-31 18:41:54 +0600

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -97,7 +97,7 @@ export default function Footer() {
         </div>
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 LUXE. All rights reserved.</p>
+          <p>&copy; 2025 ARVANA. All rights reserved.</p>
         </div>
       </div>
     </footer>
